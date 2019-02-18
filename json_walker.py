6e97f92e59171172c18cabf59f411b16cfa67217
Julia <pochynok@ucu.edu.ua>
2019-02-18 20:38:08 +0200

    Add files

---
+++ json_walker.py
@@ -0,0 +1,61 @@
+import twitter1
+import json
+
+
+def main():
+    '''
+    '''
+    twt_main = twitter1.twitter1_main()
+    fl = open("twitter1_data.json", encoding = 'utf-8')
+    data = json.load(fl)
+    fl.close()
+    print(data)
+    our_loc = data
+    if type(our_loc) == list:
+        numbers = [nums for nums in range(len(our_loc))]
+        print("Enter which element of list you need(enter number from ", numbers, "): ")
+        num_loc = int(input())
+        our_loc = our_loc[num_loc]
+        print(our_loc)
+
+    while True:
+        curr_loc = location(our_loc)
+        if curr_loc == 'back':
+            our_loc = data[1]
+            print(our_loc)
+        elif curr_loc == False:
+            break
+        elif curr_loc == 'unknown':
+            print("There is no element with this name or this name is not a key")
+        else:
+            print(curr_loc)
+            if type(curr_loc) == list:
+                numbers = [nums for nums in range(len(curr_loc))]
+                print("Enter which element of list you need(enter number from ", numbers, "): ")
+                num_loc = int(input())
+                our_loc = curr_loc[num_loc]
+                print(our_loc)
+            else:
+                our_loc = curr_loc
+        
+
+
+def location(current_loc):
+    '''
+    '''
+    loc = input("Enter where you want to go now: ")
+    if loc == 'back' or loc == "Back" or loc == "BACK":
+        return 'back'
+    elif len(loc) < 1:
+        return False
+    elif loc not in current_loc:
+        return 'unknown'
+    elif type(current_loc) == list:
+        return current_loc[loc]
+    elif type(current_loc) == dict and loc in current_loc:
+        return current_loc[loc]
+    else:
+        return 'unknown'
+
+
+main()
---
+++ twitter1.py
@@ -0,0 +1,34 @@
+import urllib.request, urllib.parse, urllib.error
+import twurl
+import ssl
+
+# https://apps.twitter.com/
+# Create App and get the four strings, put them in hidden.py
+
+def twitter1_main():
+    TWITTER_URL = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+
+    # Ignore SSL certificate errors
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
+    n = 0
+    while (n < 1):
+        print('')
+        acct = input('Enter Twitter Account:')
+        if (len(acct) < 1): break
+        url = twurl.augment(TWITTER_URL,
+                            {'screen_name': acct, 'count': '2'})
+        print('Retrieving', url)
+        connection = urllib.request.urlopen(url, context=ctx)
+        data = connection.read().decode()
+        print(data[:250])
+        headers = dict(connection.getheaders())
+        # print headers
+        print('Remaining', headers['x-rate-limit-remaining'])
+        n += 1
+
+    fl = open("twitter1_data.json", 'w')
+    fl.write(data)
+    fl.close()
